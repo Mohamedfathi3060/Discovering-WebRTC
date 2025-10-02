b7b6e93788d01ca44ee4a688bab2159bb0c14d82
Mohamed Fathi <Mohamedfathi11119@gmail.com>
2025-10-02 22:59:34 +0300

    Update WorkFlow

--- server.js
+++ server.js
@@ -62,3 +62,21 @@ wss.on('connection', (ws) => {
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => console.log('Server listening on', PORT));
+
+/*
+ TODO remove the  Accept/Reject options
+
+ -> first one joined to Room X
+ will not make any connections(Offer)
+
+ -> Everyone joined after Joined Room X After the First one WILL
+  recieve all Room X members (IDs)
+  create connection for each member(ID) Except Me
+  create Offer for each Member(ID)
+  "AUTO" All offers Will be Accepted
+
+-> for each Member in Room X recieved the Joiner's Offer
+  will create him a special Connection & Answer
+
+  So Web Service Must keep {ID -> ws} pairs to easily send Specific Messages "not Always broadcast"
+*/
